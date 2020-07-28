486d5a358edf552c2c0a80e80de24a55a87ada12
TheBeeProgrammer <43771367+TheBeeProgrammer@users.noreply.github.com>
2020-07-28 15:14:08 -0500

    db migration

--- HomeWorkNotes/app/src/main/java/com/example/homeworknotes/storage/database/NotesDatabase.kt
+++ HomeWorkNotes/app/src/main/java/com/example/homeworknotes/storage/database/NotesDatabase.kt
@@ -3,16 +3,27 @@ package com.example.homeworknotes.storage.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.homeworknotes.model.NotesModel
 import com.example.homeworknotes.storage.database.dao.NotesDao
 
 
 
-@Database(entities = [NotesModel::class], version = 2,exportSchema = false)
+@Database(entities = [NotesModel::class], version = 3,exportSchema = false)
 @TypeConverters(NotesConverter::class)
 
 abstract class NotesDatabase : RoomDatabase() {
 
     abstract fun notesDao(): NotesDao
 
+}
+
+
+val migration_2_3 = object : Migration(2, 3) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE NotesModel ADD COLUMN tasks TEXT NOT NULL DEFAULT ''"
+        )
+    }
 }
